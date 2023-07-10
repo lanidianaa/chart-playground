833641484b0fb5f4001a51fc7c03fb41023d01cf
Lani Diana <lanidianatjiam@gmail.com>
2023-07-10 20:57:49 +0700

    feat: goal line tooltip

--- src/components/goalLine.js
+++ src/components/goalLine.js
@@ -1,104 +1,178 @@
 import {
-	Chart as ChartJS,
-	CategoryScale,
-	LinearScale,
-	PointElement,
-	LineElement,
-	Title,
-	Tooltip,
-	Filler,
-	Legend,
-} from "chart.js";
-import { Line } from "react-chartjs-2";
-import gradient from "chartjs-plugin-gradient";
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from 'chart.js'
+import { Line } from 'react-chartjs-2'
+import gradient from 'chartjs-plugin-gradient'
 
 ChartJS.register(
-	CategoryScale,
-	LinearScale,
-	PointElement,
-	LineElement,
-	Title,
-	Tooltip,
-	Filler,
-	Legend,
-	gradient
-);
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+  gradient
+)
 
 const GoalLineChart = (props) => {
-	const options = {
-		responsive: true,
-		plugins: {
-			legend: {
-				position: "bottom",
-				labels: {
-					color: "#1E1C0F",
-					font: {
-						size: 11,
-						family: "Quicksand",
-						weight: "450",
-					},
-					padding: 20,
-					pointStyle: "rectRounded",
-					usePointStyle: true,
-				},
-			},
-			title: {
-				display: false,
-				text: "Chart.js Bar Chart",
-			},
-			tooltip: {
-				enabled: false,
-			},
-		},
-		scales: {
-			x: {
-				display: true,
-				ticks: {
-					display: true,
-					color: "#78776F",
-					font: {
-						size: 10,
-						family: "Quicksand",
-						weight: "600",
-					},
-					padding: 10,
-					align: "start",
-					autoSkip: true,
-				},
-				grid: {
-					drawTicks: true,
-					color: "#E8EBEE",
-				},
-			},
-			y: {
-				position: "right",
-				ticks: {
-					display: true,
-					color: "#78776F",
-					font: {
-						size: 10,
-						family: "Quicksand",
-						weight: "600",
-					},
-					padding: 10,
-					callback: function (value, index, ticks) {
-						return value;
-					},
-				},
-				grid: {
-					color: "#E8EBEE",
-					drawTicks: false,
-				},
-			},
-		},
-	};
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'bottom',
+        labels: {
+          color: '#1E1C0F',
+          font: {
+            size: 11,
+            family: 'Quicksand',
+            weight: '450',
+          },
+          padding: 20,
+          pointStyle: 'rectRounded',
+          usePointStyle: true,
+        },
+      },
+      title: {
+        display: false,
+        text: 'Chart.js Bar Chart',
+      },
+      tooltip: {
+        enabled: false,
+        intersect: false,
+        usePointStyle: false,
+        external: ({ chart, tooltip }) => {
+          const ct = document.getElementById('custom-tooltip')
+          const cth = document.getElementById('tooltip-title')
+          // current
+          const ctcc = document.getElementById('tooltip-current-color')
+          const ctc0 = document.getElementById('tooltip-current-0')
+          const ctc1 = document.getElementById('tooltip-current-1')
+          // target
+          const cttc = document.getElementById('tooltip-target-color')
+          const ctt0 = document.getElementById('tooltip-target-0')
+          const ctt1 = document.getElementById('tooltip-target-1')
 
-	return (
-		<Line
-			options={options}
-			{...props}
-		/>
-	);
-};
+          // show custom tooltip
+          ct.style.opacity = 1
+          // change label / title for tooltip
+          cth.innerHTML = tooltip?.dataPoints[0].label
 
-export default GoalLineChart;
+          const dataCurrent = tooltip?.dataPoints?.find(
+            (item) => item.dataset.label === 'Current'
+          )
+          if (dataCurrent) {
+            // change current value
+            ctc1.innerHTML = dataCurrent.formattedValue ?? 0
+            ctcc.style.opacity = 1
+            ctc0.style.opacity = 1
+            ctc1.style.opacity = 1
+          } else {
+            ctcc.style.opacity = 0
+            ctc0.style.opacity = 0
+            ctc1.style.opacity = 0
+          }
+          const dataTarget = tooltip?.dataPoints?.find(
+            (item) => item.dataset.label === 'Target'
+          )
+          if (dataTarget) {
+            // change and show target value
+            ctt1.innerHTML = dataTarget.formattedValue ?? 0
+            cttc.style.opacity = 1
+            ctt0.style.opacity = 1
+            ctt1.style.opacity = 1
+          } else {
+            cttc.style.opacity = 0
+            ctt0.style.opacity = 0
+            ctt1.style.opacity = 0
+          }
+
+          return ''
+        },
+      },
+    },
+    events: ['mousemove', 'mouseout', 'mouseleave'],
+    onHover: (evt, _, chart) => {
+      if (evt.type === 'mouseleave' || evt.type === 'mouseout') {
+        console.log(evt, chart, _)
+      }
+
+      //   Change x gridline color based on hovered data index
+      if (_ && _.length) {
+        const xIndex = _[0].index
+        chart.scales.x._gridLineItems?.forEach((grids, id) => {
+          if (id === xIndex) {
+            grids.color = '#FB9825'
+            grids.width = 3
+          } else {
+            grids.color = '#E8EBEE'
+            grids.width = 1
+          }
+        })
+      } else {
+        chart.scales.x._gridLineItems?.forEach((grids) => {
+          grids.color = '#E8EBEE'
+          grids.width = 1
+        })
+      }
+    },
+    interaction: {
+      intersect: false,
+      mode: 'index',
+    },
+    scales: {
+      x: {
+        display: true,
+        ticks: {
+          display: true,
+          color: '#78776F',
+          font: {
+            size: 10,
+            family: 'Quicksand',
+            weight: '600',
+          },
+          padding: 10,
+          align: 'start',
+          autoSkip: true,
+        },
+        grid: {
+          drawTicks: true,
+          color: '#E8EBEE',
+        },
+      },
+      y: {
+        position: 'right',
+        ticks: {
+          display: true,
+          color: '#78776F',
+          font: {
+            size: 10,
+            family: 'Quicksand',
+            weight: '600',
+          },
+          padding: 10,
+          callback: function (value, index, ticks) {
+            return value
+          },
+        },
+        grid: {
+          color: '#E8EBEE',
+          drawTicks: false,
+        },
+      },
+    },
+  }
+
+  return <Line options={options} {...props} />
+}
+
+export default GoalLineChart
--- src/pages/chart/goal-line.js
+++ src/pages/chart/goal-line.js
@@ -1,55 +1,113 @@
-import GoalLineChart from "@/components/goalLine";
+import GoalLineChart from '@/components/goalLine'
 
 const GoalLine = () => {
-	const dataFromServer = [10, null, null, null, 50];
-	const max = dataFromServer.reduce((a, b) => Math.max(a, b));
+  const dataFromServer = [10, null, null, null, 50]
+  const max = dataFromServer.reduce((a, b) => Math.max(a, b))
 
-	const data = {
-		labels: ["Jan", "Apr", "Jul", "Oct", "Nov"],
-		datasets: [
-			{
-				fill: true,
-				label: "Current",
-				data: [10, 14, 36],
-				// borderColor: "rgb(53, 162, 235)",
-				pointRadius: 0,
-				backgroundColor: "#F1652875",
-				borderColor: "#F16528",
-				borderWidth: 0,
-			},
-			{
-				fill: true,
-				label: "Target",
-				data: [10, null, null, null, 50],
-				borderColor: "#0FAD61",
-				pointRadius: 0,
-				backgroundColor: "#0FAD61",
-				borderCapStyle: "round",
-				borderDash: [7],
-				spanGaps: true,
-				gradient: {
-					backgroundColor: {
-						axis: "y",
-						colors: {
-							0: "#D9D9D900",
-							[max]: "#0C9955",
-						},
-					},
-				},
-			},
-		],
-	};
-	return (
-		<div className="w-screen h-screen flex justify-center items-center">
-			<div className="border border-rose-200 rounded-lg p-10">
-				<GoalLineChart
-					data={data}
-					width={320}
-					height={320}
-				/>
-			</div>
-		</div>
-	);
-};
+  const data = {
+    labels: ['Jan', 'Apr', 'Jul', 'Oct', 'Nov'],
+    datasets: [
+      {
+        fill: true,
+        label: 'Current',
+        data: [10, 14, 36],
+        // borderColor: "rgb(53, 162, 235)",
+        pointRadius: 0,
+        backgroundColor: '#F1652875',
+        borderColor: '#F16528',
+        borderWidth: 0,
+      },
+      {
+        fill: true,
+        label: 'Target',
+        data: [10, 20, 30, 40, 50],
+        borderColor: '#0FAD61',
+        pointRadius: 0,
+        backgroundColor: '#0FAD61',
+        borderCapStyle: 'round',
+        borderDash: [7],
+        spanGaps: true,
+        gradient: {
+          backgroundColor: {
+            axis: 'y',
+            colors: {
+              0: '#D9D9D900',
+              [max]: '#0C9955',
+            },
+          },
+        },
+      },
+    ],
+  }
 
-export default GoalLine;
+  const resetTooltip = () => {
+    const ct = document.getElementById('custom-tooltip')
+    if (ct) ct.style.opacity = 0
+  }
+
+  return (
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div
+        className='border border-rose-200 rounded-lg p-10'
+        onMouseLeave={() => resetTooltip()}
+      >
+        <div className='relative min-h-[32px]'>
+          <div
+            id='custom-tooltip'
+            className='absolute z-10 top-0 transition-all ease-in-out duration-100 opacity-0 w-full'
+            onTouchStart={() => resetTooltip()}
+            onClick={() => resetTooltip()}
+          >
+            <div className='bg-gray-1 flex flex-col justify-center items-center rounded-lg px-4 py-2'>
+              <p
+                id='tooltip-title'
+                className='text-[10px] font-semibold tracking-wide leading-4 mb-1 text-center text-[#78776F]'
+              ></p>
+              <div className='flex items-center justify-around w-full'>
+                <div className='flex gap-1 items-center'>
+                  <span
+                    id='tooltip-current-color'
+                    className='bg-[#F1652875] h-3 w-3 -mt-0.5 rounded-sm opacity-0'
+                  ></span>
+                  <span
+                    id='tooltip-current-0'
+                    className='text-[10px] tracking-wide leading-4 font-semibold capitalize opacity-0'
+                  >
+                    Portfolio:
+                  </span>
+                  <span
+                    id='tooltip-current-1'
+                    className='text-[10px] tracking-wide leading-4 font-semibold opacity-0'
+                  >
+                    100,000,000
+                  </span>
+                </div>
+                <div className='flex gap-1 items-center'>
+                  <span
+                    id='tooltip-target-color'
+                    className='bg-[#0FAD61] h-3 w-3 -mt-0.5 rounded-sm opacity-0'
+                  ></span>
+                  <span
+                    id='tooltip-target-0'
+                    className='text-[10px] tracking-wide leading-4 font-semibold capitalize opacity-0'
+                  >
+                    Target:
+                  </span>
+                  <span
+                    id='tooltip-target-1'
+                    className='text-[10px] tracking-wide leading-4 font-semibold opacity-0'
+                  >
+                    100,000,000
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <GoalLineChart data={data} width={320} height={320} />
+      </div>
+    </div>
+  )
+}
+
+export default GoalLine
